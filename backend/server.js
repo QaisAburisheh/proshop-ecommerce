1fb55895fc6dd11699a519acc3a0e19410f7d773
QaisAburisheh <qais.aburisheh@gmail.com>
2024-08-07 15:43:32 +0300

    fix server route in server.js

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 //Cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("api is running");
-});
+
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -49,10 +47,9 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
-  const __dirname = path.resolve();
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
 }
 
 // errorHandler
