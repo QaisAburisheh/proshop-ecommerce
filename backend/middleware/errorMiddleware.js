ec3a903600a5c0c8594e137a53e3a28a285f9d1e
QaisAburisheh <qais.aburisheh@gmail.com>
2024-08-07 15:37:50 +0300

    update server.js file

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,4 +1,4 @@
-const notfound = (req, res, next) => {
+const notFound = (req, res, next) => {
   const error = new Error(`Not Found: ${req.originalUrl}`);
   res.status(404);
   next(error);
@@ -13,13 +13,10 @@ const errorHandler = (err, req, res, next) => {
     message = `Resource not found`;
     statusCode = 404;
   }
-  res
-    .status(statusCode)
-    .json({
-      message,
-      stack: process.env.NODE_ENV === "production" ? "🎂" : err.stack,
-    });
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === "production" ? "🎂" : err.stack,
+  });
 };
 
-
-export {notfound, errorHandler} 
+export { notFound, errorHandler };
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
-import { notfound, errorHandler } from "./middleware/errorMiddleware.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 
 const port = process.env.PORT || 5000;
@@ -56,7 +56,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // errorHandler
-app.use(notfound);
+app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => {
