c29d0a51ef09d294a0be2b75ace91bee100953a6
QaisAburisheh <qais.aburisheh@gmail.com>
2024-08-07 15:27:04 +0300

    update server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-import path from "path";
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 dotenv.config();
@@ -8,44 +8,57 @@ import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import { notfound, errorHandler } from "./middleware/errorMiddleware.js";
+
 
 const port = process.env.PORT || 5000;
 
 connectDB();
-
 const app = express();
 
+// Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//Cookie parser middleware
 app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
+// PayPal routes
 app.get("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-if (process.env.NODE_ENV === "production") {
+// path
+const __dirname = path.resolve(); //set __dirname to current  directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
-
+  const __dirname = path.resolve();
   app.get("/", (req, res) => {
     res.send("API is running....");
   });
 }
 
-app.use(notFound);
+// errorHandler
+app.use(notfound);
 app.use(errorHandler);
 
-app.listen(port, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
-);
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
